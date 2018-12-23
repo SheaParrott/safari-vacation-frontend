9b36eefa067278346bbdadfb5fc761668be0e223
SheaParrott <alexandergotthis@gmail.com>
2018-12-22 23:56:28 -0500

    started safari front end

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import axios from 'axios'
 
 class App extends Component {
+  CORS_ANYWHERE_URL = 'https://cors-anywhere.herokuapp.com/'
+  constructor(props) {
+    super(props)
+    this.state = {
+      animals: ''
+    }
+  }
+  componentDidMount = () => {
+    axios.get(`http://localhost:4567/Animals`).then(response => {
+      console.log(response.data)
+      this.setState({
+        animals: response.data
+      })
+    })
+  }
+  showAnimals = () => {
+    console.log(this.state.animals)
+    // return this.state.animals.map((animal, index) => {
+    //   return <p key={index}>{animal.species}</p>
+    // })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Safari Vacation</h1>
+        {this.showAnimals()}
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
