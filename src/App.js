34e5601225310d821da39f1ce98a857d9cfb7fc8
SheaParrott <alexandergotthis@gmail.com>
2019-01-04 19:13:26 -0500

    all tasks completed. some CSS added

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import './App.css'
 import axios from 'axios'
 
 // [x] Finish all the endpoints from the Safari API
-// [] Create a simple react app, that uses some css make it looks friendly.
+// [x] Create a simple react app, that uses some css make it looks friendly.
 // The react app should:
 // [x] Display all animals the user has seen
 // [x]Display all animals seen in the Jungle
 // [x] Remove all animals that I have seen in the Desert.
 //    - done this through state manipulation
 //    - this will ensure you can still see all animals seen if desired
-// [] Add all the CountOfTimesSeen and get a total number of animals seen
-// []Get the CountOfTimesSeen of lions, tigers and bears
+// [x] Add all the CountOfTimesSeen and get a total number of animals seen
+// [x]Get the CountOfTimesSeen of lions, tigers and bears
 
 class App extends Component {
   CORS_ANYWHERE_URL = 'https://cors-anywhere.herokuapp.com/'
@@ -20,6 +20,8 @@ class App extends Component {
     this.state = {
       animalsShown: [],
       allAnimals: [],
+      numberOfAnimalsSeen: '',
+      numberOfLionsTigersAndBears: '',
       animalsNotSeenInDesert: [],
       jungleAnimals: []
     }
@@ -28,6 +30,22 @@ class App extends Component {
   componentDidMount = () => {
     this.getAllAnimals()
     this.GetAnimalsSeenInJungle()
+    this.getCountOfAnimalsSeen()
+    this.getCountOfLionsTigersAndBears()
+  }
+  getCountOfLionsTigersAndBears = () => {
+    axios.get(`http://localhost:4567/Count/LTB`).then(response => {
+      this.setState({
+        numberOfLionsTigersAndBears: response.data
+      })
+    })
+  }
+  getCountOfAnimalsSeen = () => {
+    axios.get(`http://localhost:4567/Count`).then(response => {
+      this.setState({
+        numberOfAnimalsSeen: response.data
+      })
+    })
   }
   getAllAnimals = () => {
     axios.get(`http://localhost:4567/Animals`).then(response => {
@@ -92,16 +110,20 @@ class App extends Component {
         <h1>Safari Vacation</h1>
         <h4>
           Did you have a fun time on the Safari tour? I sure hope so. Here is a
-          list of the animals you seen on the tour!
+          list of the animals you seen on the tour! You saw{' '}
+          {this.state.numberOfAnimalsSeen.total_count_of_times_seen} animals on
+          this tour.{' '}
+          {this.state.numberOfLionsTigersAndBears.total_count_of_times_seen_LTB}{' '}
+          of these were Lions, Tigers and Bears.
         </h4>
         {this.showAnimals()}
-        <h4>Here are all the animals you seen in the jungle</h4>
-        {this.showAllAnimalsSeenInJungle()}
         <h4>
-          I remeber the desert was not your favorite part of the tour. Click the
-          button below to remove them from your list
+          Since the desert was not your favorite part of the tour. Click the
+          button below to remove them from your list above.
         </h4>
         <button onClick={this.deleteAllDesertAnimals}>submit</button>
+        <h4>All the animals you saw in the jungle:</h4>
+        {this.showAllAnimalsSeenInJungle()}
       </div>
     )
   }
